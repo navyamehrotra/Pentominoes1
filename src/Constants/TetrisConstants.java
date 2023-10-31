4eeba200e05fe1d16cfc1f160903e709dd660c25
tymotylis <tymek@baranscy.info>
2023-10-31 17:17:43 +0100

    Phase 2 code structure

---
+++ src/Constants/TetrisConstants.java
@@ -0,0 +1,9 @@
+package Constants;
+
+public class TetrisConstants {
+    // Time difference between each board update in ms
+    public static final double TICK_DELTA = 500;
+
+    public static final int BOARD_WIDTH = 5;
+    public static final int BORD_HEIGHT = 15;
+}
--- src/Phase1/ExperimentGenerator.java
+++ src/Phase1/ExperimentGenerator.java
@@ -1,3 +1,4 @@
+package Phase1;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
