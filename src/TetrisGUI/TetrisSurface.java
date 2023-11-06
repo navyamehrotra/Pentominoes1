15b8133abcd6736a8fe0bf95126bcc50283e89a0
Vjosa Shabanaj <v.shabanaj@student.maastrichtuniversity.nl>
2023-11-06 18:56:15 +0100

    an attempt at pentomino visualisation with Ata

--- src/TetrisGUI/TetrisSurface.java
+++ src/TetrisGUI/TetrisSurface.java
@@ -2,20 +2,28 @@ package TetrisGUI;
 
 import TetrisControllers.BoardController;
 
-public class TetrisSurface {
-    private BoardController boardController;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-    public TetrisSurface(BoardController boardController) {
+import Phase1.PentominoBuilder;
+import Phase1.PentominoDatabase;
+import Phase1.UI;
+
+public class TetrisSurface extends JPanel {
+    private BoardController boardController;
+    private UI ui = new UI(5, 15, 50);
+
+    public TetrisSurface(BoardController boardController, UI ui) {
         this.boardController = boardController;
+        this.ui = ui;
     }
 
     public Object getCell(int row, int column) {
         int id = boardController.getIDInCell(row, column);
 
-        //...
+        // ...
 
         return null;
     }
 
-    
 }
