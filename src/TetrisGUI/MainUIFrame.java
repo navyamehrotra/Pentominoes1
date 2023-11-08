86b624dcad112f6ccc5e27036f10e8f558921444
Vjosa Shabanaj <v.shabanaj@student.maastrichtuniversity.nl>
2023-11-08 17:28:59 +0100

    perfected the menus with Navya :)

--- src/TetrisGUI/MainUIFrame.java
+++ src/TetrisGUI/MainUIFrame.java
@@ -1,31 +1,38 @@
 package TetrisGUI;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+
 
 public class MainUIFrame {
- 
+
     private BoardUI boardUI;
     private ScoreUI scoreUI;
 
     public MainUIFrame(BoardUI boardUI, ScoreUI scoreUI) {
         this.boardUI = boardUI;
         this.scoreUI = scoreUI;
-        String[] colNames = {"name", "score"};
-        String[][] data = {{"Aukje", "5000"}, {"Ata", "3500"}, {"Ty", "3000"}, {"Nathan", "2600"}, {"Anatoly", "2300"}};
+
+        // Faked data for testing purposes
+        String[] colNames = { "name", "score" };
+        String[][] data = { { "Aukje", "5000" }, { "Ata", "3500" }, { "Ty", "3000" }, { "Nathan", "2600" },
+                { "Anatoly", "2300" } };
+        JTable table = new JTable(data, colNames);        
 
         JFrame frame = new JFrame();
-        JTable table = new JTable(data, colNames);
+        JPanel rightPanel = new JPanel(new BorderLayout());
 
+        JButton reset = new JButton("Reset");
         JButton play = new JButton("Play");
+        JButton quit = new JButton("Quit");
 
+        // For testing purposes for play button functionality
         play.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -34,29 +41,20 @@ public class MainUIFrame {
             }
         });
 
-        JPanel imagePanel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-
-                ImageIcon image = new ImageIcon("C:\\Users\\V\\Desktop\\Project 1-1\\project-1-1\\codetest\\dalle3.png");
-                Image img = image.getImage();
-                //Image scaledImage = img.getScaledInstance(1000, 1000, Image.SCALE_SMOOTH);
-                g.drawImage(img, 0, 0, this);
-            }
-        };
-
         frame.setLayout(new BorderLayout());
 
-        frame.setSize(500, 700);
-        play.setSize(30, 10);
-
         JPanel tablePanel = new JPanel(new BorderLayout());
+        tablePanel.add(play, BorderLayout.NORTH);
         tablePanel.add(table, BorderLayout.CENTER);
-        tablePanel.add(play, BorderLayout.SOUTH);
+        tablePanel.add(quit, BorderLayout.SOUTH);
+        
+        rightPanel.add(reset, BorderLayout.NORTH);
+        rightPanel.add(tablePanel, BorderLayout.SOUTH);
 
-        frame.add(imagePanel, BorderLayout.CENTER);
-        frame.add(tablePanel, BorderLayout.SOUTH);
+        frame.add(new TetrisGrid(), BorderLayout.CENTER);
+        frame.add(rightPanel, BorderLayout.EAST);
+
+        frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
@@ -64,6 +62,6 @@ public class MainUIFrame {
     public void updateAndDisplay() {
         JPanel tetrisBoard = boardUI.update();
         JPanel highScore = scoreUI.update();
-        //...
+        // ...
     }
 }
