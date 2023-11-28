c2d9195f696513222c36860a11c885dddd9da137
navyamehrotra <67598666+navyamehrotra@users.noreply.github.com>
2023-11-28 16:36:08 +0100

    playerinput file changed only.

--- src/TetrisControllers/PlayerInput.java
+++ src/TetrisControllers/PlayerInput.java
@@ -1,5 +1,10 @@
 package TetrisControllers;
 
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
+import javax.swing.KeyStroke;
+
 public class PlayerInput {
 
     private BoardController boardController;
@@ -11,5 +16,30 @@ public class PlayerInput {
     public void readInput() {
         
     }
+    public void registerInputs(JComponent component) {
+        component.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_UP:
+                        boardController.rotatePentomino();
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        boardController.movePentominoDown();
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        boardController.movePentominoLeft();
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        boardController.movePentominoRight();
+                        break;
+                    case Keyevent.VK_SPACE:
+                        boardController.H
+                }
+            }
+        });
+
+    }
+
     
 }
