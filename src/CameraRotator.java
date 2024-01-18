3531ee65f6a373d958d3f005dfdea009fc38a01e
Aukje Heijthuijzen <aukjeheijthuijzen@MacBook-Air-van-Aukje.local>
2024-01-18 14:13:09 +0100

    Camera rotation improvement

--- src/CameraRotator.java
+++ src/CameraRotator.java
@@ -26,13 +26,17 @@ public class CameraRotator extends MouseAdapter {
     private Vector3D cameraRotation;
     private Vector3D cameraPosition;
 
+    private double[] midpoint = {0, 0, 0};
+    private double[] y = {5, 5, 5};
+    private double radius = 9.12132;
+
     public CameraRotator(Camera camera, JFrame frame) {
         frame.addMouseListener(this);
         frame.addMouseMotionListener(this);
         this.camera = camera;
 
         // Temp start values:
-        cameraPosition = new Vector3D(-10, -0, -0);
+        cameraPosition = new Vector3D(0, 0, 0);
         cameraRotation = new Vector3D(0 * Math.PI / 180, 275 * Math.PI / 180, -180 * Math.PI / 180);
 
         camera.setPosition(cameraPosition);
@@ -45,11 +49,6 @@ public class CameraRotator extends MouseAdapter {
         firstX = e.getX();
         firstY = e.getY();
 
-    }
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // 
     }
 
     @Override
@@ -78,10 +77,52 @@ public class CameraRotator extends MouseAdapter {
         cameraRotation.y += deltaX * sensitivity;
         cameraRotation.x -= deltaY * sensitivity;
 
-        double maxVertAngle = Math.toRadians(89.0);
+        double maxVertAngle = Math.toRadians(359.0);
         cameraRotation.x = Math.max(-maxVertAngle, Math.min(maxVertAngle, cameraRotation.x));
+
+        double[] x = findX(midpoint, y, radius);
+
+        cameraPosition.x = x[0];
+        cameraPosition.y = x[1];
+        cameraPosition.z = x[2];
 
         camera.setRotation(cameraRotation);
         camera.setPosition(cameraPosition);
     }
+
+    public static double[] findX(double[] midpoint, double[] y, double radius) {
+        double[] vector = new double[3];
+        
+        for (int i = 0; i < 3; i++) {
+            vector[i] = y[i] - midpoint[i];
+        }
+
+        double norm = calculateNorm(vector);
+
+        double[] directionVector = new double[3];
+        for (int i = 0; i < 3; i++) {
+            directionVector[i] = vector[i] / norm;
+        }
+
+        double[] x = new double[3];
+        for (int i = 0; i < 3; i++) {
+            x[i] = midpoint[i] + radius * directionVector[i];
+        }
+
+        return x;
+    }
+
+    public static double calculateNorm(double[] vector) {
+        double sumOfSquares = 0.0;
+
+        for(double value : vector) {
+            sumOfSquares += value * value;
+        }
+
+        return Math.sqrt(sumOfSquares);
+    }
 }
+
+
+
+
