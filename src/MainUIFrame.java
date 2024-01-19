d569797445ca4c978757cc1ff5e356b433135610
tymotylis <tymek@baranscy.info>
2024-01-19 20:59:48 +0100

    Made UI Prettier

--- src/MainUIFrame.java
+++ src/MainUIFrame.java
@@ -16,13 +16,21 @@ public class MainUIFrame {
         //ImageIcon resetIcon  = new ImageIcon(System.getProperty("user.dir") + "/src/assets/reset.png"); 
         //ImageIcon modesIcon = new ImageIcon(System.getProperty("user.dir") + "/src/assets/human.png");
 
+        javax.swing.plaf.FontUIResource font = new javax.swing.plaf.FontUIResource("Arial", Font.PLAIN, 25);
+        UIManager.put("Button.font", font);
+        UIManager.put("ToggleButton.font", font);
+        UIManager.put("Label.font", font);
+        UIManager.put("Spinner.font", font);
+
+
         frame = new JFrame();
         JPanel buttonPanel = new JPanel();
-        JButton reset = new JButton("Reset Angle");
+        JButton reset = new JButton("Reset View");
+        
         JButton compute = new JButton("Compute");
-        JButton resetAll = new JButton("Reset");
-        JToggleButton modes = new JToggleButton("Modes");
-        JToggleButton parcel = new JToggleButton("Poss Mode");
+        //JButton resetAll = new JButton("Reset");
+        JToggleButton modes = new JToggleButton("Board Fill Mode");
+        JToggleButton parcel = new JToggleButton("ABC Parcels Mode");
 
         mainPanel = new ImagePanel(bgImage);
         frame.setContentPane(mainPanel);
@@ -50,63 +58,98 @@ public class MainUIFrame {
         c.fill = GridBagConstraints.NONE;
         frame.add(reset, c);
 
-        JLabel label = new JLabel("test");
+        JLabel label = new JLabel("...");
 
         bottomPanel.add(label, BorderLayout.NORTH);
         bottomPanel.add(buttonPanel, BorderLayout.CENTER);
         bottomPanel.setOpaque(true);
         bottomPanel.setBackground(new Color(255, 255, 255));
 
-        buttonPanel.setLayout(new GridLayout(1, 4));
-        buttonPanel.add(compute);
+        buttonPanel.setLayout(new GridLayout(1, 4, 10, 10));
         buttonPanel.add(modes);
-        buttonPanel.add(resetAll);
+        //buttonPanel.add(resetAll);
         buttonPanel.add(parcel);
+        buttonPanel.add(compute);
+
         buttonPanel.setOpaque(true);
         buttonPanel.setBackground(new Color(255, 255, 255));
 
         JSpinner valueA = new JSpinner();
         JSpinner valueB = new JSpinner();
         JSpinner valueC = new JSpinner();
+        valueA.setValue(3);
+        valueB.setValue(4);
+        valueC.setValue(5);
 
-        JSpinner sizeX = new JSpinner();
-        JSpinner sizeY = new JSpinner();
-        JSpinner sizeZ = new JSpinner();
+        SpinnerNumberModel modelX = new SpinnerNumberModel(16.5, 0.5, 40.0, 0.5);  
+        SpinnerNumberModel modelY = new SpinnerNumberModel(2.5, 0.5, 40.0, 0.5);  
+        SpinnerNumberModel modelZ = new SpinnerNumberModel(4.0, 0.5, 40.0, 0.5);  
+        JSpinner sizeX = new JSpinner(modelX);
+        JSpinner sizeY = new JSpinner(modelY);
+        JSpinner sizeZ = new JSpinner(modelZ);
 
         JPanel topPanel = new JPanel();
 
-        topPanel.setLayout(new GridLayout(3, 4));
         c.anchor = GridBagConstraints.FIRST_LINE_START;
         c.gridx = 0;
         frame.add(topPanel, c);
+        topPanel.setLayout(new GridLayout(8, 2, 10, 0));
         topPanel.setOpaque(true);
         topPanel.setBackground(new Color(255, 255, 255));
 
-        topPanel.add(new JLabel("value A: "));
+        JLabel aLabel = new JLabel("value of A:");
+        JLabel bLabel = new JLabel("value of B:");
+        JLabel cLabel = new JLabel("value of C:");
+
+        topPanel.add(aLabel);
         topPanel.add(valueA);
-        topPanel.add(new JLabel(" size X:"));
-        topPanel.add(sizeX);
-
-        topPanel.add(new JLabel("value B: "));
+        topPanel.add(bLabel);
         topPanel.add(valueB);
-        topPanel.add(new JLabel(" size Y:"));
-        topPanel.add(sizeY);
-
-        topPanel.add(new JLabel("value C: "));
+        topPanel.add(cLabel);
         topPanel.add(valueC);
-        topPanel.add(new JLabel(" size Z:"));
+
+        topPanel.add(new JLabel());
+        topPanel.add(new JLabel());
+
+        topPanel.add(new JLabel("Truck Length:"));
+        topPanel.add(sizeX);
+        topPanel.add(new JLabel("Truck Width:"));
+        topPanel.add(sizeY);
+        topPanel.add(new JLabel("Truck Height:"));
         topPanel.add(sizeZ);
 
         //topPanel.add(valueC);
 
-        //parcel.addActionListener(e -> {
-        /*     if (!parcel.isSelected()) {
+        modes.addActionListener(e -> {
+             if (modes.isSelected()) {
                 // possibility mode is on
+                modes.setText("Max Score Mode");
             } else {
-                parcel.setText("Val Mode");
                 // value mode is on
+                modes.setText("Board Fill Mode");
             }
-        }); */
+        }); 
+
+        parcel.addActionListener(e -> {
+            if (parcel.isSelected()) {
+               // possibility mode is on
+                parcel.setText("PLT Parcels Mode");
+                aLabel.setText("Value of P:");
+                bLabel.setText("Value of L:");
+                cLabel.setText("Value of T:");
+           } else {
+            parcel.setText("ABC Parcels Mode");
+            aLabel.setText("Value of A:");
+            bLabel.setText("Value of B:");
+            cLabel.setText("Value of C:");
+
+               // value mode is on
+           }
+       }); 
+
+
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        //frame.setUndecorated(true);
         frame.setVisible(true);
     }
 
